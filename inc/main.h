5944854a54adb9c51c74c9646904bf7a5c7899d2
sfidan42 <sadettinfidan00@gmail.com>
2024-07-24 12:55:10 +0300

    modern openGL triangle

---
+++ inc/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+# define MAIN_H
+# include <GL/glew.h>
+# include <GLFW/glfw3.h>
+# include <iostream>
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,46 +1,64 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <main.h>
 
-int main(void)
+int	main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow	*window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+		std::cerr << "Error: GLFW not initialized" << std::endl;
+		return -1;
+	}
 
-    glewInit();
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Error: GLEW not initialized" << std::endl;
+		return -1;
+	}
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+	unsigned int	buffer_id;
+	float			data[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	glGenBuffers(1, &buffer_id);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), data, GL_STATIC_DRAW);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    glfwTerminate();
-    return 0;
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
