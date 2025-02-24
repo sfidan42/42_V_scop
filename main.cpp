150d207bf5bb9c45e7aea9145dfeff5ffc7269f5
sfidan42 <sadettinfidan00@gmail.com>
2025-02-24 16:27:36 +0300

    fix window resize

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <scop.hpp>
 
-float	g_width = 800.0f;
-float	g_height = 600.0f;
-float	g_hStep = 0.0f;
-float	g_vStep = 0.0f;
+float			g_width = 800.0f;
+float			g_height = 600.0f;
+float			g_hStep = 0.0f;
+float			g_vStep = 0.0f;
+unsigned int	g_shaderProgram;
 
 void	framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	(void)window;
 	g_width = width;
 	g_height = height;
-	glViewport(0, 0, width, height);
-	std::cout << "width: " << width << " height: " << height << std::endl;
+	glViewport(0, 0, g_width, g_height);
+	std::cout << "width: " << g_width << " height: " << g_height << std::endl;
+
+	glm::mat4	projection = glm::perspective(glm::radians(70.0f), g_width / g_height, 0.1f, 1000.0f);
+	glUniformMatrix4fv(glGetUniformLocation(g_shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 }
 
 void	key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -122,20 +126,20 @@ int	main(int c, char **av)
 	distance = distance * 2.0f + (5.0f / 8.0f) * (cosf(35.0f) / sinf(35.0f));
 
 	shader.parse("res/shaders/specular.shader");
-	unsigned int	shaderProgram = shader.create();
+	g_shaderProgram = shader.create();
 
 	glm::mat4	model = glm::mat4(1.0f);
 	glm::mat4	view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -distance));
 	glm::mat4	projection = glm::perspective(glm::radians(70.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
 
-	glUseProgram(shaderProgram);
-	glUniform3f(glGetUniformLocation(shaderProgram, "lightPos"), distance, -distance, -distance);
-	glUniform3f(glGetUniformLocation(shaderProgram, "ambientColor"), mat.ka.r, mat.ka.g, mat.ka.b);
-	glUniform3f(glGetUniformLocation(shaderProgram, "diffuseColor"), mat.kd.r, mat.kd.g, mat.kd.b);
-	glUniform3f(glGetUniformLocation(shaderProgram, "specularColor"), mat.ks.r, mat.ks.g, mat.ks.b);
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
-	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+	glUseProgram(g_shaderProgram);
+	glUniform3f(glGetUniformLocation(g_shaderProgram, "lightPos"), distance, -distance, -distance);
+	glUniform3f(glGetUniformLocation(g_shaderProgram, "ambientColor"), mat.ka.r, mat.ka.g, mat.ka.b);
+	glUniform3f(glGetUniformLocation(g_shaderProgram, "diffuseColor"), mat.kd.r, mat.kd.g, mat.kd.b);
+	glUniform3f(glGetUniformLocation(g_shaderProgram, "specularColor"), mat.ks.r, mat.ks.g, mat.ks.b);
+	glUniformMatrix4fv(glGetUniformLocation(g_shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix4fv(glGetUniformLocation(g_shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(glGetUniformLocation(g_shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
 	glBindVertexArray(VAOs);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -149,10 +153,10 @@ int	main(int c, char **av)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		model = glm::rotate(model, glm::radians((float)M_PI / 5.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(g_shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
 		view = glm::translate(view, glm::vec3(g_hStep * distance, g_vStep * distance, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(g_shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
 		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 
